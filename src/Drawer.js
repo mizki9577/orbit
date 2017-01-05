7b0f4bb8a428ada122a2e9af6962314db302eb48
Mizki SUZUMORI <mizki9577@gmail.com>
2017-01-06 03:41:19 +0900

    transform. I'm sad.

--- src/Drawer.js
+++ src/Drawer.js
@@ -44,29 +44,16 @@ class Drawer extends Component {
   }
 
   render() {
-    const { windowWidth, windowHeight, mouseX, mouseY, isMouseButtonPushed, zoomLevel, bodies } = this.state
-
-    const width  = windowWidth  / zoomLevel
-    const height = windowHeight / zoomLevel
-    const bottom  = height / 2
-    const right = width  / 2
-    const top = -bottom
-    const left = -right
-    const svgMouseX = mouseX === null ? null : left + mouseX / zoomLevel
-    const svgMouseY = mouseY === null ? null : top + mouseY / zoomLevel
+    const { windowWidth, windowHeight, zoomLevel, bodies } = this.state
 
     return (
-      <svg style={ style } width={ windowWidth } height={ windowHeight } viewBox={ `${left} ${top} ${width} ${height}` }
+      <svg style={ style } width={ windowWidth } height={ windowHeight } viewBox={ `${-windowWidth/2} ${-windowHeight/2} ${windowWidth} ${windowHeight}` }
            onMouseMove={ this.handleMouseMove.bind(this) } onMouseDown={ this.handleMouseDown.bind(this) } onMouseUp={ this.handleMouseUp.bind(this) } onMouseLeave={ this.handleMouseLeave.bind(this) } onWheel={ this.handleWheel.bind(this) }>
-        {
-          bodies.map(b => <circle key={ b.id } r={ b.radius } cx={ b.x } cy={ b.y } style={ b.style } />)
-        }
-        { svgMouseX === null || svgMouseY === null ? null : (
-          <g stroke={ isMouseButtonPushed ? 'red' : 'black' } strokeWidth={ 1 / zoomLevel }>
-            <line x1={ left } y1={ svgMouseY } x2={ right } y2={ svgMouseY } />
-            <line x1={ svgMouseX } y1={ top } x2={ svgMouseX } y2={ bottom } />
-          </g>
-        ) }
+        <g transform={`scale(${zoomLevel})`}>
+          {
+            bodies.map(b => <circle key={ b.id } r={ b.radius } cx={ b.x } cy={ b.y } style={ b.style } />)
+          }
+        </g>
       </svg>
     )
   }
