3c4d5a4599cf108463c2fd683742152026d0c240
Mizki SUZUMORI <mizki9577@gmail.com>
2017-01-26 23:25:10 +0900

    fix continue fails bug

--- src/actions.js
+++ src/actions.js
@@ -19,32 +19,33 @@ export const applicationStarted = () => {
 
 export const update = (timestamp: number) => {
   const { bodies, isRunning } = store.getState()
-  if (!isRunning) return
 
-  dispatcher.dispatch({
-    type: 'update',
-    timestamp,
-    bodies: bodies.map(self => {
-      let ax = 0
-      let ay = 0
+  if (isRunning) {
+    dispatcher.dispatch({
+      type: 'update',
+      timestamp,
+      bodies: bodies.map(self => {
+        let ax = 0
+        let ay = 0
 
-      for (const other of bodies) {
-        if (self === other) continue
-        const coefficient = -other.mass * ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** -1.5
-        ax += coefficient * (self.x - other.x)
-        ay += coefficient * (self.y - other.y)
-      }
+        for (const other of bodies) {
+          if (self === other) continue
+          const coefficient = -other.mass * ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** -1.5
+          ax += coefficient * (self.x - other.x)
+          ay += coefficient * (self.y - other.y)
+        }
 
-      const vx = self.vx + ax
-      const vy = self.vy + ay
-      const x = self.x + vx
-      const y = self.y + vy
+        const vx = self.vx + ax
+        const vy = self.vy + ay
+        const x = self.x + vx
+        const y = self.y + vy
 
-      //const locus = [[self.x, self.y], ...self.locus]
-      const locus = []
-      return { ...self, x, y, vx, vy, locus }
-    }),
-  })
+        //const locus = [[self.x, self.y], ...self.locus]
+        const locus = []
+        return { ...self, x, y, vx, vy, locus }
+      }),
+    })
+  }
 
   window.requestAnimationFrame(update)
 }
