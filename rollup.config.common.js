a05473867ea065c30b2f6ddeb521992a9e0075b1
Mizki SUZUMORI <mizki9577@gmail.com>
2017-01-31 15:49:32 +0900

    index.js -> main.js

--- rollup.config.common.js
+++ rollup.config.common.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'src/index.js',
+  entry: 'src/main.js',
   dest: 'public/bundle.js',
   format: 'es',
 
