23ac6a16a6ce08778f5359e1ca27be7a5848a9fd
Mizki SUZUMORI <mizki9577@gmail.com>
2017-02-04 01:04:52 +0900

    BIW: remove unused vars

--- src/components/BodyInformationWindow.js
+++ src/components/BodyInformationWindow.js
@@ -44,16 +44,6 @@ class BodyInformationWindow extends Component {
     const body = bodies.find(b => b.id === selectedBodyId)
     if (body == null) return null
 
-    const entries = [
-      ['Mass'      , 'number', body.mass],
-      ['Radius'    , 'number', body.radius],
-      ['X'         , 'number', body.x.toPrecision(4)],
-      ['Y'         , 'number', body.y.toPrecision(4)],
-      ['Velocity X', 'number', body.vx.toPrecision(4)],
-      ['Velocity Y', 'number', body.vy.toPrecision(4)],
-      ['Speed'     , 'number', Math.hypot(body.vx, body.vy).toPrecision(4)],
-    ]
-
     return (
       <div style={ style.container }>
         <table style={ style.table }>
