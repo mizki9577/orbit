09c8ac436e4c5949d53fe5c16ddb6e60108b9173
Mizki SUZUMORI <mizki9577@gmail.com>
2017-04-08 03:21:05 +0900

    I'm tired about webpack...

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,52 +2,61 @@ import { DefinePlugin } from 'webpack'
 import BabiliPlugin from 'babili-webpack-plugin'
 import path from 'path'
 
-export default env => ({
-  entry: './src/main.js',
-  devtool: env.development ? 'eval-source-map' : false,
+export default env => {
+  const isDevelopment = env.development || !env.production
+  const config = {}
 
-  output: {
+  config.entry = './src/main.js'
+  config.devtool = isDevelopment ? 'inline-source-map' : false
+
+  config.output = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
-  },
+  }
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
+  config.module = {}
+  config.module.rules = [
+    {
+      test: /\.js$/,
+      loader: 'babel-loader'
+    },
 
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              minimize: env.production,
-              sourceMap: env.development,
-            },
+    {
+      test: /\.css$/,
+      use: [
+        { loader: 'style-loader' },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            minimize: !isDevelopment,
+            sourceMap: isDevelopment,
           },
-        ],
-      },
-    ],
-  },
+        },
+      ],
+    },
+  ]
 
-  plugins: [
+  config.plugins = []
+  config.plugins.push(
     new DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env.production ? 'production' : 'development'),
-    }),
+      'process.env': {
+        NODE_ENV: JSON.stringify(!isDevelopment ? 'production' : 'development'),
+      }
+    })
+  )
 
-    new BabiliPlugin(env.production),
-  ],
+  if (!isDevelopment) {
+    config.plugins.push(new BabiliPlugin(!isDevelopment))
+  }
 
-  devServer: {
+  config.devServer = {
     contentBase: path.join(__dirname, 'public'),
     host: '0.0.0.0',
     port: 8080,
-  },
-})
+  }
+
+  return config
+}
 
 // vim: set ts=2 sw=2 et:
