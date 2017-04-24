2b1be205c0822303ffcb616f9034c02e00145c6d
Mizki SUZUMORI <mizki9577@gmail.com>
2017-04-24 19:29:48 +0900

    add deploy script for powershell

---
+++ scripts/deploy.ps1
@@ -0,0 +1,5 @@
+﻿New-Item -ItemType Directory -Name .\build\development
+npm run build:production  -- --output-path ./build --output-public-path /orbit/
+npm run build:development -- --output-path ./build/development --output-public-path /orbit/development/
+gh-pages -d build
+Remove-Item -Path .\build -Recurse
